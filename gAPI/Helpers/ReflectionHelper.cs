75eecbe46c38851e25c784cea9f949d214c9c617
Willem Beltman <hwj.beltman@gmail.com>
2025-10-12 23:36:16 +0200

    a

--- gAPI/Helpers/ReflectionHelper.cs
+++ gAPI/Helpers/ReflectionHelper.cs
@@ -1,4 +1,5 @@
-﻿using gAPI.Interfaces;
+﻿using gAPI.EntityFrameworkDisk;
+using gAPI.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
