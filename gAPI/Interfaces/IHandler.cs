74f1bd11c1efb078ce5fd34f1a8aec9bf6b21215
ik willem <hwj.beltman@gmail.com>
2026-01-11 20:13:06 +0100

    DI met services en handlers goed, rip mappings

--- gAPI/Interfaces/IHandler.cs
+++ gAPI/Interfaces/IHandler.cs
@@ -1,15 +1,15 @@
-﻿using gAPI.AutoMapper;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using gAPI.AutoMapper;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace gAPI.Interfaces;
+//namespace gAPI.Interfaces;
 
-public interface IHandler<TEntity, TDto, TKey> : ISecurityHandler<TDto>
-{
-    Task<TEntity?> FindByMatchAsync(TDto dto); 
-    Task<TEntity?> FindByIdAsync(TKey id);
-    IQueryable<TEntity> ListAll();
-    Task<bool> AddAsync(TEntity entity);
-    Task<bool> UpdateAsync(TEntity entity, TDto dto);
-    Task<bool> RemoveAsync(TEntity entity);
-}
+//public interface IHandler<TEntity, TDto, TKey> : ISecurityHandler<TDto>
+//{
+//    Task<TEntity?> FindByMatchAsync(TDto dto); 
+//    Task<TEntity?> FindByIdAsync(TKey id);
+//    IQueryable<TEntity> ListAll();
+//    Task<bool> AddAsync(TEntity entity);
+//    Task<bool> UpdateAsync(TEntity entity, TDto dto);
+//    Task<bool> RemoveAsync(TEntity entity);
+//}
