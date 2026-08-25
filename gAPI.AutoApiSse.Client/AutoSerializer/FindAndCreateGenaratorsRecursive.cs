c8cbcf2a43c5ec4e0a2fe52af738a912a026b905
Willem Beltman <hwj.beltman@gmail.com>
2026-08-25 18:12:23 +0200

    Nieuwe gAPI.AutoApiSse

---
+++ gAPI.AutoApiSse.Client/AutoSerializer/FindAndCreateGenaratorsRecursive.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace gAPI.AutoSerializer;
+
+public class FindAndCreateGenaratorsRecursive
+{
+    public static HashSet<INamedTypeSymbol> FindAndCreateGenerators(
+        INamedTypeSymbol[] typesToGenerate,
+        IEnumerable<INamedTypeSymbol> customSerializers,
+        bool skipRecord = false)
+    {
+        var visited = new HashSet<INamedTypeSymbol>(SymbolEqualityComparer.Default);
+
+        foreach (var type in typesToGenerate)
+        {
+            GetListRecursive(type, visited, customSerializers, skipRecord);
+        }
+
+        return visited;
+    }
+
+    private static void GetListRecursive(
+        INamedTypeSymbol typeSymbol,
+        HashSet<INamedTypeSymbol> visited,
+        IEnumerable<INamedTypeSymbol> customSerializers,
+        bool skipRecord)
+    {
+        if (visited.Contains(typeSymbol)) return;
+        if (customSerializers.Any(a => SymbolEqualityComparer.Default.Equals(a, typeSymbol))) return;
+        visited.Add(typeSymbol);
+
+        foreach (var prop in Helper.GetProperties(typeSymbol)) //typeSymbol.GetMembers().OfType<IPropertySymbol>())
+        {
+            if (prop.Property.SetMethod == null || prop.Property.GetMethod == null) continue;
+            if (prop.Property.GetAttributes().Any(a => a.ToString().EndsWith("NotMappedAttribute"))) continue;
+
+            // Pak underlying type als Nullable<T>
+            var propType = prop.Property.Type;
+            if (propType.OriginalDefinition.ToDisplayString() == "System.Nullable<T>" && propType is INamedTypeSymbol nts && nts.TypeArguments.Length == 1)
+            {
+                propType = nts.TypeArguments[0];
+            }
+            // Array !!!!!
+            if (propType is IArrayTypeSymbol array)
+            {
+                if (array.ElementType.Name == "Byte") continue; // Byte array doet de serializer al goed, dus skip
+                propType = array.ElementType;
+            }
+
+            if (skipRecord && (propType.IsRecord || propType.TypeKind == TypeKind.Struct))
+                continue;
+
+            if (!Helper.IsPrimitiveOrKnownType(propType) && propType is INamedTypeSymbol nestedType)
+            {
+                var customSerializer = customSerializers.FirstOrDefault(a => SymbolEqualityComparer.Default.Equals(a, nestedType));
+                if (customSerializer != null) continue;
+
+                GetListRecursive(nestedType, visited, customSerializers, skipRecord);
+            }
+        }
+    }
+}
---
+++ gAPI.AutoApiSse.Client/Models/SharedReferences.cs
@@ -0,0 +1,32 @@
+﻿using gAPI.AutoApiSse.Client.Helpers;
+using Microsoft.CodeAnalysis;
+
+namespace gAPI.AutoApiSse.Client.Models;
+
+public class SharedReferences
+{
+    public SharedReferences(INamedTypeSymbol[] allSymbols)
+    {
+        SendRequestDto = SharedReferenceFinder.Find("gAPI.Core.Dtos.SendRequestDto", allSymbols);
+        ServiceId = SharedReferenceFinder.Find("gAPI.Core.Ids.ServiceId", allSymbols);
+        SseManagerId = SharedReferenceFinder.Find("gAPI.Core.Ids.SseManagerId", allSymbols);
+        SendRequestDto = SharedReferenceFinder.Find("gAPI.Core.Dtos.SendRequestDto", allSymbols);
+        IClientAuthenticatedHttpClient = SharedReferenceFinder.Find("gAPI.Core.Client.Interfaces.IClientAuthenticatedHttpClient", allSymbols);
+        ISseClientConnection = SharedReferenceFinder.Find("gAPI.Core.Client.Interfaces.ISseClientConnection", allSymbols);
+
+        SseManagerCollection = SharedReferenceFinder.Find("gAPI.Core.Client.Collections.SseManagerCollection", allSymbols);
+        SseClient = SharedReferenceFinder.Find("gAPI.Core.Client.Sse.SseClient", allSymbols);
+
+        AuthClient_FormFile = SharedReferenceFinder.TryFindByAttribute("gAPI.Core.Attributes.IsFormFileAttribute", allSymbols);
+        AuthClient_ToFormFileExtension = SharedReferenceFinder.TryFindByAttribute("gAPI.Core.Attributes.IsFormFileExtensionAttribute", allSymbols);
+    }
+    public SharedReference ServiceId { get; }
+    public SharedReference SendRequestDto { get; }
+    public SharedReference IClientAuthenticatedHttpClient { get; }
+    public SharedReference SseManagerCollection { get; }
+    public SharedReference ISseClientConnection { get; }
+    public SharedReference SseManagerId { get; }
+    public SharedReference SseClient { get; }
+    public SharedReference? AuthClient_FormFile { get; }
+    public SharedReference? AuthClient_ToFormFileExtension { get; }
+}
