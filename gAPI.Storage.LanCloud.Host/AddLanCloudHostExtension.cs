d0d031e1400c32337be593e2093074f24d871cf4
Willem Beltman <hwj.beltman@gmail.com>
2026-09-20 06:22:12 +0200

    Het build dus het zal wel goed zijn, config ellende

--- gAPI.Storage.LanCloud.Host/AddLanCloudHostExtension.cs
+++ gAPI.Storage.LanCloud.Host/AddLanCloudHostExtension.cs
@@ -2,11 +2,17 @@
 using gAPI.Generated;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace gAPI.Storage.LanCloud.Host;
 
 public static class AddLanCloudHostExtension
 {
+    public static IServiceCollection AddLanCloudHost(
+        this HostApplicationBuilder builder)
+    {
+        return AddLanCloudHost(builder.Services, builder.Configuration);
+    }
     public static IServiceCollection AddLanCloudHost(
         this IServiceCollection services,
         IConfigurationManager configuration)
@@ -18,11 +24,12 @@ public static class AddLanCloudHostExtension
         this IServiceCollection services,
         LanCloudHostConfig config)
     {
+        services.AddSingleton(config);
+
         services.AddAutoWssClient(config);
         services.AddAutoAuthClient(config);
 
         services.AddHostedService<HostHub>();
-        services.AddSingleton(config);
         services.AddSingleton<ClientConfig>(sp => sp.GetRequiredService<LanCloudHostConfig>());
 
         return services;
