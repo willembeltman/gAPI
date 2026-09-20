39b345c2f1b54545f5cffbbc84d7aa68067421f2
Willem Beltman <hwj.beltman@gmail.com>
2026-09-20 12:20:08 +0200

    Opslaan na grote grote schoonmaak

--- gAPI.CodeGen.Backend/BackendGenerator.cs
+++ gAPI.CodeGen.Backend/BackendGenerator.cs
@@ -35,7 +35,7 @@ public class BackendGenerator
         //UserIpSessionTokenRoute = new UserIpSessionTokenRouteGenerator(this);
         //UserIpSessionTokenRouteRequest = new UserIpSessionTokenRouteRequestGenerator(this);
 
-        // Bsd.Shared.Public.Dtos 
+        // Shared.Dtos 
         Dtos = [.. DbContext.DbSets
             .Where(a => !a.Entity.IsHidden)
             .Select(dbSet => new DtoGenerator(this, dbSet))];
